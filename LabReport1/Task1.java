79f4fd72938b7bb08c1694bb6cc71a7c09cfe6b6
naimul-web <naimul2604@gmail.com>
2026-07-11 02:42:12 +0600

    Create Task2.java

---
+++ LabReport1/Task1.java
@@ -0,0 +1,13 @@
+public class Shariar {
+    public static void main(String[] args) {
+        String name = "Naimul Islam";
+        String studentId = "E253010";
+        int age = 21;
+        String phone = "01889797668";
+        String email = "Naimul@gmail.com";
+        String address = "Chakaria, Cox's bazar, Chittagong";
+
+        System.out.printf("Name:%s\nStudentId:%s\nAge:%d\nPhone:%s\nEmail:%s\nAddress:%s\n", 
+            name, studentId, age, phone, email, address);
+    }
+}
